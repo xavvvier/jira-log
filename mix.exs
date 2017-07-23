21a933952f3d196852cd2a1b4cc5305eb4e0b59a
alternar64@gmail.com <xavvvier@users.noreply.github.com>
2017-07-23 13:48:59 -0500

    Increased version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule JiraLog.Mixfile do
 
   def project do
     [app: :jira_log,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
